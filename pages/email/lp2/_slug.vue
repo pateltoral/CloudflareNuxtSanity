7f99e7c911cf565dffa0cd801642195d93d5b65a
Toral Patel <pateltoral3@gmail.com>
2022-02-16 09:14:26 -0500

    Initial commit for nuxt project

---
+++ pages/email/lp2/_slug.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="emailtemplate">
+    <h1>LANDING PAGE 2</h1>
+    <h2 class="title">{{ emailtemplate.name }}</h2>
+    <img :src="$urlFor(emailtemplate.logo)"/>
+  </div>
+  <div class="title" v-else>404</div>
+</template>
+<script>
+import { groq } from "@nuxtjs/sanity";
+import { SanityContent } from "@nuxtjs/sanity/dist/components/sanity-content";
+export default {
+  components: { SanityContent },
+  async asyncData({ params, $sanity }) {
+    const query = groq`*[_type == "emailtemplate" && commonitems.name == "${params.slug}"][0].commonitems`;
+    const emailtemplate = await $sanity.fetch(query);
+    return { emailtemplate };
+  },
+  head() {
+    return {
+      link: [
+        {
+          rel: "stylesheet",
+          href: `/${this.emailtemplate ? this.emailtemplate.name : "main"}.css`
+        }
+      ]
+    };
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">Welcome to NUXT proj</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import { groq } from '@nuxtjs/sanity'
+
+export default {
+  async asyncData({ $sanity }) {
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 2rem;
+  min-height: 100vh;
+}
+.title {
+  margin: 2rem 0;
+}
+</style>
